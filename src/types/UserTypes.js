5cd54555215764e17c8c1c1b11c6471292bd356a
bruno_henr <brunohenriquex0@gmail.com>
2024-04-12 18:53:49 -0300

    feat: criado tipagem com jsDoc

--- src/types/UserTypes.js
+++ src/types/UserTypes.js
@@ -5,9 +5,38 @@
  */
 
 /**
+ * @typedef {Object} TechnologyAddDTO
+ * @property {string} title
+ * @property {string} deadline
+*/
+
+/**
+ * @typedef {Object} TechnologyEditDTO
+ * @property {string} title
+ * @property {string} deadline
+*/
+
+/**
  * @typedef {Object} UserAddDTOReturn
  * @property {string} id
  * @property {string} name
  * @property {string} username
  * @property {Array} technologies
+ */
+
+/**
+ * @typedef {Object} Technology
+ * @property {string} id 
+ * @property {string} title 
+ * @property {boolean} studied 
+ * @property {string} deadline 
+ * @property {string} created_at 
+ */
+
+/**
+ * @typedef {Object} User
+ * @property {string} id 
+ * @property {string} name 
+ * @property {string} username 
+ * @property {Technology[]} technologies 
  */
