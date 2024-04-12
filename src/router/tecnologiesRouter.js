2e881d101986892286147b5b69fab4244bed9fc0
bruno_henr <brunohenriquex0@gmail.com>
2024-04-12 19:41:45 -0300

    feat: criado rota para marcar tecnologia como completa e deletar

--- src/router/tecnologiesRouter.js
+++ src/router/tecnologiesRouter.js
@@ -3,6 +3,8 @@ import { checkExistsUserAccount } from "../middleware/checkExistsUserAccount.js"
 import { listTecnologiesController } from "../useCases/tecnologies/list/index.js";
 import { addTecnologiesController } from "../useCases/tecnologies/add/index.js";
 import { editTecnologiesController } from "../useCases/tecnologies/edit/index.js";
+import { markTecnologyController } from "../useCases/tecnologies/markTecnology/index.js";
+import { deleteTecnologyController } from "../useCases/tecnologies/delete/index.js";
 
 export const tecnologiesRouter = Router();
 tecnologiesRouter.use(checkExistsUserAccount);
@@ -17,4 +19,11 @@ tecnologiesRouter.post("/tecnologies", (req, res) => {
 
 tecnologiesRouter.put('/tecnologies/:id', (req, res) => {
     editTecnologiesController.handle(req, res);
+})
+
+tecnologiesRouter.patch('/technologies/:id/studied', (req, res) => {
+    markTecnologyController.handle(req, res);
+})
+tecnologiesRouter.delete('/technologies/:id', (req, res) => {
+    deleteTecnologyController.handle(req, res);
 })
