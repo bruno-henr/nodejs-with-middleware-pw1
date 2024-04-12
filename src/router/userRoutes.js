2876ed6be4487722ee5a37d9401b9cd09048efe2
bruno_henr <brunohenriquex0@gmail.com>
2024-04-12 19:40:59 -0300

    feat: criado rota para listar usuarios

--- src/router/userRoutes.js
+++ src/router/userRoutes.js
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { addUserController } from "../useCases/user/add/index.js";
+import { listUserController } from "../useCases/user/list/index.js";
 
 export const userRouter = Router();
 
 userRouter.post("/users", (req, res) => addUserController.handle(req, res));
+userRouter.get("/users", (req, res) => listUserController.handle(req, res));
